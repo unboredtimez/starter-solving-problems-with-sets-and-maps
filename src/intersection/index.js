59f23cdd2795c4b259c40cd7d7977f965a71ff3e
Joshua Carrasco <josh0371@gmail.com>
2022-12-05 17:39:43 -0500

    first

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,18 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    const newMap = new Map();
+    const newSet = new Set();
+
+    for (const element of a) {
+        newMap.set(element)
+    }
+
+    for (const element of b) {
+        if (newMap.has(element) == true) {
+            newSet.add(element)
+        }
+    }
+
+    return [...newSet]
+};
 
 module.exports = intersection;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,28 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    const numbers = new Map();
+    const solution = new Map();
+
+    for (const element of A) {
+        numbers.set(element);
+
+        for (const element of A) {
+            let diff = N - element;
+
+            if (diff != element) {
+                if (numbers.has(diff) == true) {
+                    if (element < diff) {
+                        solution.set(element, diff);
+                    } else {
+                        solution.set(diff, element);
+                    };
+                };
+            };
+        };
+    };
+    return [...solution]
+};
 
 module.exports = sumPairs;
